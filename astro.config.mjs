1b381453f6b0be8202a34b6232deccbacec9c0b3
kame854397 <djwfh593@gmail.com>
2023-09-29 21:02:21 +0900

    相対パスでビルド

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
-
+import relativeLinks from 'astro-relative-links';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [relativeLinks()],
 });
